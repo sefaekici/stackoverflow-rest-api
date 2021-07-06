ee3188c3ae3c660c53a21b9c2b709110934834a3
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-07-06 12:35:30 +0300

    Add routers middleware in server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const auth = require("./routers/auth");
+const questions = require("./routers/questions");
 
 //Environment Variables
 dotenv.config({
   path: "./config/env/config.env",
 });
 
-//localhost:5000/api
-
 const app = express();
+
+//localhost:5000/api
+app.use("api/questions", questions);
+app.use("api/auth", auth);
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server started on PORT:${PORT}`));
