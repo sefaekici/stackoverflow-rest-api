4ef8c3244ce9c15b814a26619da373e7fb814d75
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-07-06 12:25:10 +0300

    Create Auth and Questions Route File

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+//Environment Variables
+dotenv.config({
+  path: "./config/env/config.env",
+});
+
+//localhost:5000/api
 
 const app = express();
-
-//5000 Portu veya ortamda uygun olan portun kullanılmasını sağlar.
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Anasayfa");
+});
+
+app.get("/api/questions", (req, res) => {
+  res.send("Hello Questions Home Page...");
+});
+
+app.get("/api/questions/delete", (req, res) => {
+  res.send("Hello Questions Delete Page...");
 });
 
 app.listen(PORT, () => console.log(`Server started on PORT:${PORT}`));
