0977f7744f6aa9591fb5dc96110e14d72cf4be7b
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-07-07 09:39:25 +0300

    Update routers

--- routers/auth.js
+++ routers/auth.js
@@ -7,8 +7,4 @@ router.get("/", (req, res) => {
   res.send("Auth Home Page");
 });
 
-router.get("/register", (req, res) => {
-  res.send("Auth Register Page");
-});
-
 module.exports = router;
--- routers/questions.js
+++ routers/questions.js
@@ -6,8 +6,4 @@ router.get("/", (req, res) => {
   res.send("Questions Home Page");
 });
 
-router.get("/delete", (req, res) => {
-  res.send("Questions Delete Page");
-});
-
 module.exports = router;
