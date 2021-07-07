52b06f33e3919901bc9481b3db816ef81277ad86
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-07-07 16:01:38 +0300

    Update error handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,23 @@
 const User = require("../models/User");
 
-const register = async (req, res, next) => {
-  const name = "Sefa Ekici2";
-  const email = "ssefaekici23@gmail.com";
+const register = (req, res, next) => {
+  const name = "Sefa Ekici245";
+  const email = "ssefaekici2356@gmail.com";
   const password = "123456";
 
   //async await
-
-  const user = await User.create({
-    name: name,
-    email: email,
-    password: password,
-  });
-
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
+  User.create({
+    name,
+    email,
+    password,
+  })
+    .then((data) => {
+      res.status(200).json({
+        success: true,
+        data: data,
+      });
+    })
+    .catch((err) => next(err)); //catch içerisnde hata yakalandı ve next parametresi yardımı ile error handlera gönderilmiş oldu.
 };
 
 module.exports = {
